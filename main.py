09065e00c899a5cd88a4b5ac63bbfb8702ef5e57
AnyCreation <131777823+AnyCreation@users.noreply.github.com>
2023-05-29 15:36:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,32 +6,36 @@ list_num = [float(i) for i in NumJump]
 NUMs = Find.Jump(list_num)
 
 
-if NUMs[0] == True and NUMs[1] == True:
-    print("The list must contain 3 elements")
+if len(NUMs) == 1:
+    if NUMs:
+        print('Jump is break')
 else:
-    if NUMs[1]:
+    if NUMs[0] == True and NUMs[1] == True:
+        print("The list must contain 3 elements")
+    else:
+        if NUMs[1]:
 
-        if NUMs[0][0]:
-            print(f'Jump == X + {list_num[1] - list_num[0]}')
+            if NUMs[0][0]:
+                print(f'Jump == X + {list_num[1] - list_num[0]}')
 
-        elif NUMs[0][1]:
-            print(f'Jump == X * {list_num[1] / list_num[0]}')
+            elif NUMs[0][1]:
+                print(f'Jump == X * {list_num[1] / list_num[0]}')
 
-        elif NUMs[0][2]:
-            print(f'Jump == X ** 2')
+            elif NUMs[0][2]:
+                print(f'Jump == X ** 2')
 
-    elif NUMs[0] and NUMs[1]:
-            print('All elements in list are the same')
-            
+        elif NUMs[0] and NUMs[1]:
+                print('All elements in list are the same')
 
-    elif NUMs[1]:
-        
-        if NUMs[0][0]:
-            print(f'Jump == X - {list_num[0] - list_num[1]}')
 
-        elif NUMs[0][1]:
-            print(f'Jump == X / {list_num[0] / list_num[1]}')
+        elif NUMs[1]:
 
-        elif NUMs[0][2]:
-            print(f'Jump == X ** 0.5')
- 
+            if NUMs[0][0]:
+                print(f'Jump == X - {list_num[0] - list_num[1]}')
+
+            elif NUMs[0][1]:
+                print(f'Jump == X / {list_num[0] / list_num[1]}')
+
+            elif NUMs[0][2]:
+                print(f'Jump == X ** 0.5')
+
